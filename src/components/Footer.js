b3c162de17752004b25c84bd1bcc5d600d1f1e43
liemro9x@gmail.com <liemro9x@gmail.com>
2023-01-29 00:55:15 +0700

    Demo1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <footer className="sticky-footer bg-white">
         <div className="container my-auto">
           <div className="copyright text-center my-auto">
-            <span>Copyright &copy; CRM Management System 2021</span>
+            <span>Copyright &copy; CRM Management System 2023 </span>
           </div>
         </div>
       </footer>
